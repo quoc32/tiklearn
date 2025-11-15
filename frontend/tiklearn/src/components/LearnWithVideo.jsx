0ee8c9806401c4a22755fbf7d015dd84c58a16dc
quoc32 <vuanhquoc0397144200@gmail.com>
2025-11-15 12:14:32 +0700

    them 7

---
+++ frontend/tiklearn/src/components/LearnWithVideo.jsx
@@ -0,0 +1,13 @@
+
+function LearnWithVideo() {
+ 
+  return (
+    <div
+      style={{ height: '90vh', width: '100vw', display: 'flex', alignItems: 'center', justifyContent: 'center', overflow: 'hidden' }}
+    >
+      <h1>Video</h1>
+    </div>
+  )
+}
+
+export default LearnWithVideo
