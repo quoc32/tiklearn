d227781df48d68363edd54c20756caf3d0648588
quoc32 <vuanhquoc0397144200@gmail.com>
2025-11-15 17:01:05 +0700

    them 10

---
+++ frontend/tiklearn/src/components/QuizCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function QuizCard({ items }) {
+  return (
+    <div className="max-w-sm bg-white rounded-lg shadow-md overflow-hidden hover:shadow-xl transition-shadow duration-200 cursor-pointer">
+      {console.log(items)}
+    </div>
+  );
+}
