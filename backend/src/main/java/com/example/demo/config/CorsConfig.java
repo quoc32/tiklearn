68c49b07bf8cf06579c23a1e38e2c137274e3938
quoc32 <vuanhquoc0397144200@gmail.com>
2025-11-15 11:23:14 +0700

    them 2

--- backend/src/main/java/com/example/demo/config/CorsConfig.java
+++ backend/src/main/java/com/example/demo/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5173") // Frontend origin
+                        .allowedOrigins("http://localhost:5173") // Frontend origin
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
